49a7af052635ab441e5d7f9d2b2156de343d7484
wanasak <u510610433@gmail.com>
2017-06-08 16:07:39 +0700

    using lambda expressions

--- LambdaExpressions/Program.cs
+++ LambdaExpressions/Program.cs
@@ -10,14 +10,22 @@ namespace LambdaExpressions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Square(4));
+            // args => expression
+            // () => ...
+            // x => ...
+            // (x, y, z) => ...
+
+            Func<int, int> square = number => number * number;
+
+            //Console.WriteLine(Square(4));
+            Console.WriteLine(square(3));
 
             Console.ReadKey();
         }
 
-        static int Square(int number)
-        {
-            return number * number;
-        }
+        //static int Square(int number)
+        //{
+        //    return number * number;
+        //}
     }
 }
