2b8bc855951521171b63940e5e3f995ed46d0a23
wanasak <u510610433@gmail.com>
2017-06-08 16:00:45 +0700

    init lambda expressions

---
+++ LambdaExpressions/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Square(4));
+
+            Console.ReadKey();
+        }
+
+        static int Square(int number)
+        {
+            return number * number;
+        }
+    }
+}
